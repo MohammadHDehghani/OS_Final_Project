404502e4436f190c76bbb0c3f07fc3e03c0cc411
Mohammad Hossein Dehghani <mhda1380@gmail.com>
2023-01-22 02:07:58 +0330

    Phase2 Completed.

--- RightSide.py
+++ RightSide.py
@@ -1,21 +1,24 @@
-from multiprocessing import  Process, Queue, Value,Array
+from multiprocessing import  Process, Queue, Value,Array, Semaphore
 from time import sleep
 from Car import Car
 
 
 
-def producer(queue, id,):
+def producer(sem, queue, id,):
     print('Producer: Running', flush=True)
     while True:
+        sem.acquire()
         value = Car(id.value)
         id.value += 1
         sleep(0.5)
         queue.put(value)
+        sem.release()
 
 
-def consumer(queue, street):
+def consumer(sem, queue, street):
     print('Consumer: Running', flush=True)
     while True:
+        sem.acquire()
         item = queue.get()     
         street.value = item.id
         print('car id: ', item.id, 'sleep: ', item.time)
@@ -23,5 +26,6 @@ def consumer(queue, street):
         sleep(item.time)
         if temp != street.value:
             print('Process conflict!')
+        sem.release()
    
 
--- phase2.py
+++ phase2.py
@@ -1,10 +1,11 @@
-from multiprocessing import Process, Queue, Value,Array
+from multiprocessing import Process, Queue, Value, Semaphore
 import  RightSide, LeftSide
 
 
 
 def main():
-
+    sem1 = Semaphore(1)
+    sem2 = Semaphore(1)
     street = Value('d',0)
     id = Value('d',1)
     
@@ -12,14 +13,14 @@ def main():
     car_list2 = Queue(maxsize=10)
 
 
-    prod1 = Process(target=RightSide.producer, args=(car_list1, id))
-    cons1 = Process(target=RightSide.consumer, args=(car_list1, street))
+    prod1 = Process(target=RightSide.producer, args=(sem1, car_list1, id))
+    cons1 = Process(target=RightSide.consumer, args=(sem2, car_list1, street))
     prod1.start()
     cons1.start()
 
 
-    prod2 = Process(target=LeftSide.producer, args=(car_list2, id))
-    cons2 = Process(target=LeftSide.consumer, args=(car_list2, street))
+    prod2 = Process(target=LeftSide.producer, args=(sem1, car_list2, id))
+    cons2 = Process(target=LeftSide.consumer, args=(sem2, car_list2, street))
     prod2.start()
     cons2.start()
 
